529e3383cffeb8b95b59db39df136a6e5a03b46c
Elisabeth Giryes <elisabeth.giryes@uzh.ch>
2025-11-24 10:28:25 +0100

    add multi inst lang distr analysis + swiss unis dict

---
+++ swiss_universities.py
@@ -0,0 +1,25 @@
+""" 
+A dictionary of Swiss universities and their corresponding OpenAlex institution IDs
+
+To find the OpenAlex ID for an institution:
+1. Search for your Institution on https://openalex.org/institutions
+2. Click on it
+3. Look at the URL: "https://openalex.org/institutions/i35440088" 
+and copy the last part, which is the ID 
+
+"""
+
+swiss_unis = {
+    "ETH Zurich": "i35440088",
+    "University of Zurich": "i202697423",
+    "EPFL": "i5124864",
+    "University of Bern": "i118564535",
+    "University of Basel": "i1850255",
+    "University of St. Gallen": "i202963720",
+    "University of Lucerne": "i161941770",
+    "University of Fribourg": "i154338468",
+    "University of Lausanne": "i97565354",
+    "University of Geneva": "i114457229",
+    "University of Neuchâtel": "i57825437",
+    "Università della Svizzera italiana": "i57201433"
+}
